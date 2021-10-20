ad0f24dae4f4d7400829624c8e25cba20b09ae10
6102bitcoin <sixoneohtwo@protonmail.com>
2021-10-20 22:04:22 +0100

    Format Meetups Warning

--- pages/meetups.vue
+++ pages/meetups.vue
@@ -2,18 +2,23 @@
 	<div id="meetups-page">
 
 		<h1 class="page-title">Meetups</h1>
+		
+		<div class="getting-started-section">
 
-		<p> It is impossible to verify the authenticity of all meetups, please: </p>
-		<ul>
-			<li>Go with a friend for security</li>
-			<li>Do your own research before attending</li>
-			<li>Do not reveal personal information</li>
-			<li>Do not tell anyone how much bitcoin you own</li>
-			<li>Do <a href="https://github.com/bitcoin-only/bitcoin-only/issues/new?assignees=&labels=update&template=update-suggestion.md&title=Update+Suggestion%3A+NAME+OF+CONTENT+TO+BE+UPDATED" target="_blank">raise an issue</a> if you have a bad experience</li>
-		</ul>
+					<h3>Getting Started</h3>
 
-		<p> To get your meetup added <a href="https://github.com/bitcoin-only/bitcoin-only/issues/new?assignees=&labels=meetup&template=meetup-suggestion.md&title=Meetup+Suggestion%3A+%2A%2AExample+Bitcoin+Meetup%2A%2A" target="_blank">raise an issue</a>.</p>
+					<p> It is impossible to verify the authenticity of all meetups, please: </p>
+					<ul>
+						<li>Go with a friend for security</li>
+						<li>Do your own research before attending</li>
+						<li>Do not reveal personal information</li>
+						<li>Do not tell anyone how much bitcoin you own</li>
+						<li>Do <a href="https://github.com/bitcoin-only/bitcoin-only/issues/new?assignees=&labels=update&template=update-suggestion.md&title=Update+Suggestion%3A+NAME+OF+CONTENT+TO+BE+UPDATED" target="_blank">raise an issue</a> if you have a bad experience</li>
+					</ul>
 
+					<p> To get your meetup added <a href="https://github.com/bitcoin-only/bitcoin-only/issues/new?assignees=&labels=meetup&template=meetup-suggestion.md&title=Meetup+Suggestion%3A+%2A%2AExample+Bitcoin+Meetup%2A%2A" target="_blank">raise an issue</a>.</p>
+		</div>
+		
 		<h3>Cyberspace</h3>
 
 		<div class="tbl-scroller">
